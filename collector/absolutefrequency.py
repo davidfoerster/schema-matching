6c61e1000f3f87501b6e45a2715bd26a3b83b407
David Foerster <david.foerster@informatik.hu-berlin.de>
2014-07-07 00:02:15 +0200

    Use defaultdict for absolute frequency collector

--- collector/absolutefrequency.py
+++ collector/absolutefrequency.py
@@ -1,3 +1,4 @@
+import collections
 from collector import ItemCollector
 
 
@@ -5,12 +6,12 @@ class ItemNumericAbsoluteFrequencyCollector(ItemCollector):
 
   def __init__(self, previous_collector_set = None):
     ItemCollector.__init__(self, previous_collector_set)
-    self.absolute_frequencies = {}
+    self.absolute_frequencies = collections.defaultdict(int)
 
 
   def collect(self, item, collector_set=None):
-    current_absolute_frequency = self.absolute_frequencies.get(item, 0) + 1
-    self.absolute_frequencies[item] = current_absolute_frequency
+    self.absolute_frequencies[item] += 1
+
 
   def get_result(self, collector_set=None):
     return self.absolute_frequencies
