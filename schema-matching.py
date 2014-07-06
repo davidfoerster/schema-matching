761c17d2ae8ca6630a3e23eb400106bf50da0528
Mario Völker <voelker.mario@googlemail.com>
2014-07-06 02:11:52 +0200

    added letter average to output

--- schema-matching.py
+++ schema-matching.py
@@ -3,7 +3,7 @@ from __future__ import print_function
 import csv, sys
 from collector import *
 from collector.count import ItemCountCollector
-
+from collector.letteraverage import ItemLetterAverageCollector
 
 class UnexpectedLineException(Exception):
   def __init__(self, idx, content):
@@ -16,7 +16,7 @@ def main(*argv):
     sys.stdout = open(argv[3], 'w')
 
   # TODO: Find useful collector set(s)
-  data = [collect(path, ItemCountCollector) for path in argv[1:3]]
+  data = [collect(path, ItemCountCollector, ItemLetterAverageCollector) for path in argv[1:3]]
 
   # TODO: Analyse collector results
   print(str(data[0][1]), str(data[1][1]), sep='\n', file=sys.stderr)
