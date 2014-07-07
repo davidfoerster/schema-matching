5a157372587ce1ddd72c8e3995b1d9f7db5c578f
David Foerster <david.foerster@informatik.hu-berlin.de>
2014-07-07 05:48:53 +0200

    Fix ItemLetterRelativeFrequencyCollector
    - Add required result dependency
    - Floating point division
    - Pass collector set to used dependencies
    - Use dict instead of a generator object as the base for string
    conversion

--- collector/relativeletterfrequency.py
+++ collector/relativeletterfrequency.py
@@ -1,3 +1,4 @@
+from __future__ import division
 import utilities
 from collector import ItemCollector
 from collector.lettercount import ItemLetterCountCollector
@@ -5,12 +6,21 @@ from collector.absoluteletterfrequency import ItemLetterAbsoluteFrequencyCollect
 
 
 class ItemLetterRelativeFrequencyCollector(ItemCollector):
+
+  result_dependencies = (ItemLetterAbsoluteFrequencyCollector,)
+
+
   def __init__(self, previous_collector_set):
     ItemCollector.__init__(self, previous_collector_set)
     self.letter_count = previous_collector_set[ItemLetterCountCollector].get_result()
     self.absolute_letter_frequencies = utilities.ProbabilityDistribution()
 
 
-  def get_result(self, collector_set):
-    return {(k, v / self.letter_count)
-            for k, v in collector_set[ItemLetterAbsoluteFrequencyCollector].items()}
+  def get_result(self, collector_set = None):
+    return ((char, frequency / self.letter_count)
+      for char, frequency in
+        collector_set[ItemLetterAbsoluteFrequencyCollector].get_result(collector_set).iteritems())
+
+
+  def as_str(self, collector_set = None):
+    return str(dict(self.get_result(collector_set)))
