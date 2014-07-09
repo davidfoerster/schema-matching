c4f4a3a60940a99fa1b66ca57aa3491b55bf4771
David Foerster <david.foerster@informatik.hu-berlin.de>
2014-07-09 04:48:56 +0200

    Refactoring

--- schema-matching.py
+++ schema-matching.py
@@ -18,8 +18,17 @@ class UnexpectedLineException(Exception):
 
 
 def main(*argv):
-  if len(argv) > 3 and argv[3] != '-':
-    sys.stdout = open(argv[3], 'w')
+  """
+  :param argv: tuple[str]
+  :return: int
+  """
+  in_paths = [argv[0], argv[1]]
+  validate = False
+  if len(argv) > 2:
+    if argv[2] == '--validate':
+      validate = True
+    elif argv[2] != '-':
+      sys.stdout = open(argv[2], 'w')
 
   # collector phase description
   collectors = (
@@ -34,12 +43,13 @@ def main(*argv):
   )
 
   # collect from both input files
-  collectors = [collect(path, *collectors) for path in argv[1:3]]
+  collectors = [collect(path, *collectors) for path in in_paths]
 
   # The first collector shall have the most columns.
   reversed = len(collectors[0].merged_predecessors) < len(collectors[1].merged_predecessors)
   if reversed:
     collectors.reverse()
+    in_paths.reverse()
 
   # analyse collected data
   norms = MultiphaseCollector.results_norms(*collectors)
@@ -47,9 +57,9 @@ def main(*argv):
     print(*norms, sep='\n', end='\n\n', file=sys.stderr)
 
   # find minimal combination
-  bestmatch = get_best_schema_mapping(norms)
+  best_match = get_best_schema_mapping(norms)
   if __debug__:
-    print('norm:', bestmatch[0], file=sys.stderr)
+    print('norm:', best_match[0], file=sys.stderr)
 
   # prepare mapping for print
   offset = 1
@@ -69,7 +79,8 @@ def collect(path, *phase_descriptions):
   Collects info about the columns of the data set in file "path" according
   over multiple phases based on a description of those phases.
   """
-  print(path, end=':\n', file=sys.stderr)
+  if __debug__:
+    print(path, end=':\n', file=sys.stderr)
 
   with open(path, 'r') as f:
     multiphasecollector = MultiphaseCollector(
@@ -129,4 +140,4 @@ def get_best_schema_mapping(distance_matrix):
 
 
 if __name__ == '__main__':
-  sys.exit(main(*sys.argv))
+  sys.exit(main(*sys.argv[1:]))
