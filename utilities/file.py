f3c420227704d4921149573be21319d7f6cfea6e
David Foerster <david.foerster@informatik.hu-berlin.de>
2014-07-10 12:57:58 +0200

    Refactoring

--- utilities/file.py
+++ utilities/file.py
@@ -3,11 +3,15 @@ import itertools, sys, codecs
 from .string import DecodableUnicode
 
 
+def is_write_mode(mode):
+  return 'w' in mode or 'a' in mode
+
+
 __openspecial_names = {'/dev/std' + s: getattr(sys, 'std' + s) for s in ('in', 'out', 'err')}
 
 def openspecial(path, mode='r', *args):
   if path == '-':
-    return sys.stdout if 'w' in mode else sys.stdin
+    return sys.stdout if is_write_mode(mode) else sys.stdin
   else:
     f = __openspecial_names.get(path)
     return open(path, mode, *args) if f is None else f
