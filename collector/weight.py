c6834a1f30cf7e8c380d6fcc5e27b54f23373da9
David Foerster <david.foerster@informatik.hu-berlin.de>
2014-07-12 01:48:43 +0200

    Collector weight dictionary
    - Wrap weights in functor
    - Defaults to identity function
    - Allow transformations on sums, e. g. for L-norms

---
+++ collector/weight.py
@@ -0,0 +1,68 @@
+import math, utilities
+import utilities.iterator as uiterator
+import utilities.operator as uoperator
+from itertools import imap
+from math import expm1, fsum
+
+
+class WeightDict(dict):
+
+  class WeightFunctor(object):
+
+    __cache = dict()
+
+    def __new__(cls, weight):
+      return \
+        weight if type(weight) is cls else \
+          cls.__cache.get(weight) or object.__new__(cls)
+
+    def __init__(self, weight):
+      object.__init__(self)
+      self.weightfn = weight if callable(weight) else weight.__mul__
+      self.for_infinity = self.weightfn(utilities.infinity)
+      self.__cache.setdefault(weight, self)
+
+    def __call__(self, x):
+      return self.weightfn(x)
+
+
+  def __new__(cls, *args, **kwargs):
+    return dict.__new__(cls)
+
+
+  def __init__(self, default=uoperator.identity, sum=(math.fabs, uoperator.identity), *args, **kwargs):
+    dict.__init__(self)
+    self.default = WeightDict.WeightFunctor(default)
+    self.sum_data = sum
+    uiterator.stareach(self.__setitem__, args)
+    uiterator.stareach(self.__setitem__, kwargs.iteritems())
+
+
+  def __missing__(self, key):
+    return self.default
+
+
+  def __setitem__(self, key, value):
+    dict.__setitem__(self, key, WeightDict.WeightFunctor(value))
+
+
+  def setdefault(self, k, d=uoperator.identity):
+    dict.setdefault(self, k, WeightDict(d))
+
+
+  def sum(self, iterable):
+    return self.sum_data[1](fsum(imap(self.sum_data[0], iterable)))
+
+
+
+def normalize_exp(x):
+  """
+  Returns
+     -(exp(-x) - 1)
+   = -(exp(-x) - 1)
+   = 1 - exp(-x)
+
+  This gives us a nicely normalised distance measure that penalises large
+  values subproportionally.
+  """
+  return -expm1(-x)
