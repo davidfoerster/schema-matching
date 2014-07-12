c346ed061c1940e07d2f54613e3fcf88c90fe220
David Foerster <david.foerster@informatik.hu-berlin.de>
2014-07-12 14:19:21 +0200

    L2-normalised collector set weights

---
+++ collector/description/normal/L2.py
@@ -0,0 +1,9 @@
+from __future__ import absolute_import
+import math, utilities.operator
+from ...weight import WeightDict, normalize_exp
+from .L1 import phase_description
+
+
+# Normalised distances and L2-normalised (Euclidean norm) collector sets
+collector_weights = \
+  WeightDict(normalize_exp, (utilities.operator.square, math.sqrt))
