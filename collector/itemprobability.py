cbe677b958f53fc981b338d37bec6d2ac806582f
David Foerster <david.foerster@informatik.hu-berlin.de>
2014-07-15 13:32:34 +0200

    Provide a common base class for probability collectors

--- collector/itemprobability.py
+++ collector/itemprobability.py
@@ -1,34 +1,9 @@
 from __future__ import absolute_import, division
-import utilities.string
-from . import ItemCollector
+from .probability import BaseProbabilityCollector
 from .itemcount import ItemCountCollector
 from .itemfrequency import ItemFrequencyCollector
 
 
-class ItemProbabilityCollector(ItemCollector):
+class ItemProbabilityCollector(BaseProbabilityCollector):
 
   result_dependencies = (ItemCountCollector, ItemFrequencyCollector)
-
-
-  def __init__(self, previous_collector_set):
-    ItemCollector.__init__(self, previous_collector_set)
-    self.__cached_result = None
-
-
-  def get_result(self, collector_set):
-    if self.__cached_result is None:
-      self.__cached_result = \
-        collector_set[ItemFrequencyCollector].get_result(collector_set) \
-          .normalise(collector_set[ItemCountCollector].get_result(collector_set))
-    return self.__cached_result
-
-
-  def as_str(self, collector_set, number_fmt=''):
-    return u'({})'.format(u', '.join((
-      u'{}: {:{}}'.format(utilities.string.format_char(char), frequency, number_fmt)
-      for char, frequency in self.get_result(collector_set))))
-
-
-  @staticmethod
-  def result_norm(a, b):
-    return a.distance_to(b)
---
+++ collector/probability.py
@@ -0,0 +1,30 @@
+from __future__ import absolute_import
+from . import ItemCollector
+
+
+class BaseProbabilityCollector(ItemCollector):
+
+  # result_dependencies = (*CountCollector, *FrequencyCollector)
+
+
+  def __init__(self, previous_collector_set):
+    ItemCollector.__init__(self, previous_collector_set)
+    self.__cached_result = None
+
+
+  def get_result(self, collector_set):
+    if self.__cached_result is None:
+      self.__cached_result = \
+        collector_set[self.result_dependencies[1]].get_result(collector_set) \
+          .normalize(collector_set[self.result_dependencies[0]] \
+            .get_result(collector_set))
+    return self.__cached_result
+
+
+  def as_str(self, collector_set, number_fmt=''):
+    return format(self.get_result(collector_set), number_fmt)
+
+
+  @staticmethod
+  def result_norm(a, b):
+    return a.distance_to(b)
