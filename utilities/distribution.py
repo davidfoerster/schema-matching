5d81f168649cd41ad52ebf28a6356286241dc1b4
David Foerster <david.foerster@informatik.hu-berlin.de>
2014-07-15 13:18:52 +0200

    Class for uniform distributions

--- utilities/distribution.py
+++ utilities/distribution.py
@@ -1,8 +1,9 @@
 from __future__  import absolute_import, division
-import numbers
+import numbers, array, itertools, operator, math
 from collections import defaultdict
 from math import fsum, fabs
 from utilities.string import join, format_char
+
 
 
 class DistributionTable(object):
@@ -15,11 +16,14 @@ class DistributionTable(object):
     return NotImplemented
 
 
+  def __str__(self):
+    return self.__format__('.4g')
+
+
   def normalize(self, count=None):
     if count is None:
       count = self.count()
-    else:
-      assert count == self.count()
+    assert count == self.count()
     return self / count
 
 
@@ -52,9 +56,8 @@ class SparseDistributionTable(DistributionTable, defaultdict):
     :param divisor: numbers.Real
     :return: SparseDistributionTable
     """
-    coerced = coerce(self.default_factory(), divisor)
-    assert coerced is not None
-    return SparseDistributionTable(type(coerced[0]),
+    divisor = float(divisor)
+    return SparseDistributionTable(float,
       ((k, v / divisor) for k, v in self.iteritems()))
 
 
@@ -66,5 +69,124 @@ class SparseDistributionTable(DistributionTable, defaultdict):
       ')')
 
 
-  def __str__(self):
-    return self.__format__()
+
+class UniformBinDistributionTable(DistributionTable):
+
+  def __init__(self, start, stop, bincount=None, datatype=None, initializer=None):
+    DistributionTable.__init__(self)
+
+    assert stop > start
+    if bincount is None:
+      bincount = int(math.ceil(stop - start))
+    assert isinstance(bincount, numbers.Integral) and bincount >= 1
+    self.lower = start
+    self.upper = stop
+    self.step = (stop - start) / bincount
+
+    if initializer is None:
+      initializer = itertools.repeat(0, bincount)
+    self.data = list(initializer) if datatype is None else array.array(datatype, initializer)
+    excess = len(self.data) - bincount
+    if excess > 0:
+      del self.data[bincount:]
+    elif excess < 0:
+      self.data.extend(itertools.repeat(0, -excess))
+
+
+  def datatype(self):
+    return self.data.typecode if isinstance(self.data, array.array) else None
+
+
+  def bincount(self):
+    return len(self.data) + 1
+
+
+  def getbinlimits(self, binidx):
+    return binidx * self.step + self.lower, (binidx + 1) * self.step + self.lower
+
+
+  def getbinidx(self, key):
+    if key <= self.lower:
+      return 0
+    if key >= self.upper:
+      return len(self.data) - 1
+    else:
+      return int((key - self.lower) / self.step)
+
+
+  def __getitem__(self, key):
+    return self.data[self.getbinidx(key)]
+
+
+  def __setitem__(self, key, value):
+    self.data[self.getbinidx(key)] = value
+
+
+  def increase(self, key, value):
+    self.data[self.getbinidx(key)] += value
+
+
+  def __len__(self):
+    return len(self.data)
+
+
+  def __iter__(self):
+    return iter(self.data)
+
+
+  def count(self):
+    return sum(self)
+
+
+  def __truediv__(self, divisor):
+    return UniformBinDistributionTable(self.lower, self.upper, self.bincount(), 'd',
+      itertools.imap(float(divisor).__rdiv__, self.data))
+
+
+  def distance_to(self, compare_to):
+    if isinstance(compare_to, UniformBinDistributionTable):
+      assert all((getattr(self, k) == getattr(compare_to, k) for k in ('lower', 'upper', 'step')))
+      compare_to = compare_to.data
+    assert not hasattr(compare_to, '__len__') or len(self.data) == len(compare_to)
+    return fsum(itertools.imap(fabs, itertools.imap(operator.sub, self.data, compare_to)))
+
+
+  def __format__(self, number_format_spec=''):
+    return join('(',
+      u', '.join((
+        u'{2:{0}}-{3:{0}}: {1:{0}}'.format(
+          number_format_spec, frequency, *self.getbinlimits(bin_idx))
+        for bin_idx, frequency in enumerate(self))),
+      ')')
+
+
+  @staticmethod
+  def for_count(count, lower, upper, *args):
+    """ uses Sturge's rule: ceil(1 + log2(count)) """
+    return UniformBinDistributionTable(
+      lower, upper, _sturges_rule(count), *args)
+
+
+  @staticmethod
+  def for_variance(count, lower, upper, variance, *args):
+    """ uses Scott's rule, limited by the double of Sturge's """
+    h = int(math.ceil(3.49 * math.sqrt(variance) / _cubicroot(count)))
+    return UniformBinDistributionTable(
+      lower, upper, min(h, 2 * _sturges_rule(count)), *args)
+
+
+  @staticmethod
+  def for_quartiles(count, lower, upper, q1, q3, *args):
+    """ uses Freedman's and Diaconis' rule """
+    h = int(math.ceil(2.0 * (q3 - q1) / _cubicroot(count)))
+    return UniformBinDistributionTable(
+      lower, upper, min(h, 2 * _sturges_rule(count)), *args)
+
+
+
+def _sturges_rule(n):
+  return (n - 1).bit_length() + 1
+
+
+def _cubicroot(x):
+  return x ** (1. / 3.)
