7cf37b966049cfc47ef200ad8ae69763d98185c5
David Foerster <david.foerster@informatik.hu-berlin.de>
2014-07-17 00:19:39 +0200

    Update secondary collector description module

--- collector/description/normal/L2.py
+++ collector/description/normal/L2.py
@@ -1,10 +1,10 @@
 from __future__ import absolute_import
 import math, utilities.operator
 from ...weight import WeightDict, normalize_exp
-from .L1 import phase_description
+from .L1 import descriptions
 
 
 # Normalised distances and L2-normalised (Euclidean norm) collector sets
-collector_weights = \
+weights = \
   WeightDict(normalize_exp, (utilities.operator.square, math.sqrt),
     tags=('normalized'))
