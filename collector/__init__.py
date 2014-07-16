64ca7b1422449870b8674580b4f681d4866b8bb2
David Foerster <david.foerster@informatik.hu-berlin.de>
2014-07-16 19:43:48 +0200

    Tags for item collector sets

--- collector/__init__.py
+++ collector/__init__.py
@@ -97,6 +97,40 @@ class ItemCollector(object):
 
 
 
+class TagCollector(ItemCollector):
+
+  def __init__(self, id, data=None):
+    ItemCollector.__init__(self)
+    self.set_collected()
+    self.set_transformed()
+    self.__id = id
+    self.data = data
+
+
+  @property
+  def id(self):
+    return self.__id
+
+
+  def as_str(self, collector_set, format_spec=None):
+    return utilities.string.join(str(self.__id), ': ', str(self.data))
+
+
+  def __eq__(self, other):
+    return (self.__id == other or
+      (isinstance(other, TagCollector) and self.__id == other.__id))
+
+
+  def __ne__(self, other): return not self.__eq__(other)
+
+  def __hash__(self): return hash(self.__id)
+
+  def get_result(self, collector_set): return None
+
+  def get_type(self, collector_set): return self
+
+
+
 class ItemCollectorSet(ItemCollector, collections.OrderedDict):
   """Manages a set of collectors for a single column"""
 
