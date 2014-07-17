2337aff75a554ad1209fb95125f11907f4896d5f
David Foerster <david.foerster@informatik.hu-berlin.de>
2014-07-17 13:23:14 +0200

    Split up main function
    - Non-global output file

--- schema-matching.py
+++ schema-matching.py
@@ -23,13 +23,8 @@ number_format = '.3e'
 __interrupted = False
 
 
-def main(*argv, **kwargs):
-  """
-  :param argv: tuple[str]
-  :return: int
-  """
-  time_limit = infinity
 
+def main(argv, time_limit=None):
   # parse arguments
   argv = collections.deque(argv)
 
@@ -41,14 +36,14 @@ def main(*argv, **kwargs):
     action = None
     global __interrupted
     __interrupted = False
-    time_limit = kwargs.get('time_limit')
-    if time_limit:
+    if time_limit > 0:
       signal.signal(signal.SIGALRM, __timeout_handler)
       if signal.alarm(time_limit):
         raise RuntimeError('Who set the alarm before us?!!')
 
   # input files
-  in_paths = [argv.popleft(), argv.popleft()]
+  in1 = argv.popleft()
+  in2 = argv.popleft()
 
   # output file
   if argv:
@@ -56,18 +51,42 @@ def main(*argv, **kwargs):
     if (len(argv[0]) > len(forbidden_suffix) and
         argv[0][-len(forbidden_suffix):] == forbidden_suffix
     ):
-      print('Error: To prevent usage errors, we don\'t allow writing to *{} files.' \
+      print("Error: To prevent usage errors, we don't allow writing to *{} files." \
         .format(forbidden_suffix), file=sys.stderr)
       return 2
-    sys.stdout = utilities.file.openspecial(argv.popleft(), 'w')
+    out = utilities.file.openspecial(argv.popleft(), 'w')
+  else:
+    out = sys.stdout
+
+  if action is not None:
+    out = utilities.file.fix_file_encoding(out)
+
+  # read and analyse data
+  rv = schema_matching(action, in1, in2, argv, out)
+  if (__interrupted):
+    print(
+      'The time limit of', time_limit, 'seconds was reached. '
+      'The results may be incomplete or inaccurate.',
+      file=sys.stderr)
+  return rv
+
+
+
+def schema_matching(action, in1, in2, collector_descriptions=None, out=sys.stdout):
+  in_paths = [in1, in2]
+
+  if collector_descriptions is None:
+    collector_descriptions = ()
+  elif collector_descriptions:
+    collector_descriptions = collections.deque(collector_descriptions)
 
   # determine collector descriptions to use
-  collector_description = \
-    get_collector_description(argv.popleft() if argv else None)
+  collector_description = get_collector_description(
+    collector_descriptions.popleft() if collector_descriptions else None)
 
   # read and analyse data
   collectors, isreversed, best_match = \
-    collect_analyse_match(in_paths, collector_description)
+    collect_analyse_match(in_paths, collector_description, out)
   best_match_norm, best_match = best_match
   if isreversed:
     in_paths.reverse()
@@ -76,25 +95,23 @@ def main(*argv, **kwargs):
   if action is None:
     if verbosity >= 1:
       print('norm:', format(best_match_norm, number_format), file=sys.stderr)
-    print_result(best_match, isreversed)
-    if (__interrupted):
-      print('The time limit of', time_limit, 'seconds was reached. The results may be incomplete or inaccurate.',
-        file=sys.stderr)
+    print_result(best_match, isreversed, out)
 
   elif action == 'validate':
     invalid_count, impossible_count, missing_count = \
-      validate_result(in_paths, best_match, best_match_norm)
+      validate_result(in_paths, best_match, best_match_norm, out)
     return int(bool(invalid_count | missing_count))
 
   elif action == 'compare-descriptions':
-    return compare_descriptions(in_paths, collectors, argv,
-      (collector_description, best_match_norm, best_match))
+    return compare_descriptions(in_paths, collectors, collector_descriptions,
+      (collector_description, best_match_norm, best_match), out)
 
   else:
     print('Unknown action:', action, file=sys.stderr)
     return 2
 
   return 0
+
 
 
 def get_collector_description(srcpath=None):
@@ -127,14 +144,15 @@ def get_collector_description(srcpath=None):
   return collector_description
 
 
-def collect_analyse_match(collectors, collector_descriptions):
+
+def collect_analyse_match(collectors, collector_descriptions, out=sys.stdout):
   """
   :param collectors: list[basestring | MultiphaseCollector]
   :param collector_descriptions: object
   :return: list[MultiphaseCollector], bool, tuple[int]
   """
   assert isinstance(collectors, collections.Sequence) and len(collectors) == 2
-  collect_functor = memberfn(collect, *collector_descriptions.descriptions)
+  collect_functor = memberfn(collect, collector_descriptions.descriptions, out)
 
   if isinstance(collectors[0], MultiphaseCollector):
     assert isinstance(collectors[1], MultiphaseCollector)
@@ -160,7 +178,8 @@ def collect_analyse_match(collectors, collector_descriptions):
   return collectors, isreversed, get_best_schema_mapping(norms)
 
 
-def collect(src, *collector_descriptions):
+
+def collect(src, collector_descriptions, out=sys.stdout):
   """
   Collects info about the columns of the data set in file "path" according
   over multiple phases based on a description of those phases.
@@ -192,6 +211,7 @@ def collect(src, *collector_descriptions):
 
 def print_phase_results(multiphasecollector):
   print(multiphasecollector.merged_predecessors.as_str(number_format), file=sys.stderr)
+
 
 
 def get_best_schema_mapping(distance_matrix):
@@ -239,7 +259,8 @@ def get_best_schema_mapping(distance_matrix):
   return sweep_row(0, maxI - maxJ)
 
 
-def validate_result(in_paths, found_mappings, norm, offset=1):
+
+def validate_result(in_paths, found_mappings, norm, out=sys.stdout, offset=1):
   """
   :param in_paths: list[str]
   :param found_mappings: list[int]
@@ -277,7 +298,8 @@ def validate_result(in_paths, found_mappings, norm, offset=1):
       invalid_count += operator.ne(*original_mapping)
 
     print('found {2} => {3}, expected {2} => {0} -- {1}'.format(
-      expected, 'ok' if found_mapping[1] == expected else 'MISMATCH!', *found_mapping))
+      expected, 'ok' if found_mapping[1] == expected else 'MISMATCH!', *found_mapping),
+      file=out)
 
   # find missing matches
   missing_count = 0
@@ -289,12 +311,14 @@ def validate_result(in_paths, found_mappings, norm, offset=1):
       missing_count += 1
 
   print('\n{} invalid, {} impossible, and {} missing matches, norm = {:{}}'.format(
-    invalid_count, impossible_count, missing_count, norm, number_format))
+    invalid_count, impossible_count, missing_count, norm, number_format),
+    file=out)
 
   return invalid_count, impossible_count, missing_count
 
 
-def compare_descriptions(in_paths, collectors, to_compare, desc=None):
+
+def compare_descriptions(in_paths, collectors, to_compare, desc=None, out=sys.stdout):
   """
   :param collectors: list[basestring | MultiphaseCollector]
   :param to_compare: tuple[basestring]
@@ -312,16 +336,16 @@ def compare_descriptions(in_paths, collectors, to_compare, desc=None):
         return 2
 
     invalid_count, _, missing_count = \
-      validate_result(in_paths, best_match, best_match_norm)
-    print_description_comment(desc)
+      validate_result(in_paths, best_match, best_match_norm, out)
+    print_description_comment(desc, out)
     descriptions.append((desc, invalid_count + missing_count, best_match_norm))
 
   for desc in imap(get_collector_description, to_compare or (None,)):
-    collectors, _, best_match = collect_analyse_match(collectors, desc)
+    collectors, _, best_match = collect_analyse_match(collectors, desc, out)
     best_match_norm, best_match = best_match
     invalid_count, _, missing_count = \
-      validate_result(in_paths, best_match, best_match_norm)
-    print_description_comment(desc)
+      validate_result(in_paths, best_match, best_match_norm, out)
+    print_description_comment(desc, out)
     descriptions.append((desc, invalid_count + missing_count, best_match_norm))
 
   i = 1
@@ -329,14 +353,16 @@ def compare_descriptions(in_paths, collectors, to_compare, desc=None):
   descriptions.sort(key=utilities.operator.getitemfn(slice(1, 3)))
   for desc in descriptions:
     print(u'{}. {}, errors={}, norm={:{}}'.format(
-      i, desc[0].__file__, desc[1], desc[2], number_format))
+      i, desc[0].__file__, desc[1], desc[2], number_format),
+      file=out)
     i += last_error_count != desc[1]
     last_error_count = desc[1]
 
   return 0
 
 
-def print_result(column_mappings, reversed=False, offset=1):
+
+def print_result(column_mappings, reversed=False, out=sys.stdout, offset=1):
   """
   :param column_mappings: list[int]
   :param reversed: bool
@@ -351,13 +377,15 @@ def print_result(column_mappings, reversed=False, offset=1):
   ]
   if reversed:
     column_mappings.reverse()
-  print(*imap(','.join, izip(*column_mappings)), sep='\n')
+  print(*imap(','.join, izip(*column_mappings)), sep='\n', file=out)
 
 
-def print_description_comment(desc):
+
+def print_description_comment(desc, out):
   print(u'... with collector descriptions and weights from {} ({}).'.format(
     desc.__file__, desc.__name__),
-    end='\n\n')
+    end='\n\n', file=out)
+
 
 
 def __timeout_handler(signum, frame):
@@ -366,10 +394,11 @@ def __timeout_handler(signum, frame):
     __interrupted = timeit.default_timer() if __debug__ else True
 
 
+
 if __name__ == '__main__':
   assert DecodableUnicode.default_encoding.upper() == 'UTF-8'
   sys.stderr = utilities.file.fix_file_encoding(sys.stderr)
-  rv = main(*sys.argv[1:], time_limit=default_timelimit)
+  rv = main(sys.argv[1:], default_timelimit)
   if __debug__ and __interrupted:
     print('INFO:', timeit.default_timer() - __interrupted,
       'seconds between interruption and program termination.',
