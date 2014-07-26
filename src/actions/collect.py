eaa885835c8cfb6a5c71a31fbab5a794b5adffd7
David Foerster <david.foerster@informatik.hu-berlin.de>
2014-07-26 23:45:31 +0200

    Close schema instance files after reading

--- src/actions/collect.py
+++ src/actions/collect.py
@@ -2,6 +2,7 @@ import sys, os.path, csv
 from functools import partial as partialfn
 from utilities.iterator import map_inplace
 from utilities.functional import memberfn
+from utilities.operator import noop
 from collector.multiphase import MultiphaseCollector
 
 
@@ -38,9 +39,10 @@ def read_schema_instance(src, field_delimiter=',', verbosity=0, **kwargs):
   src_name = getattr(src, 'name', None)
   src_name = '<unknown schema instance>' if src_name is None else os.path.basename(src_name)
   reader = map(partialfn(map_inplace, str.strip),
-    csv.reader(src, delimiter=field_delimiter,
-      skipinitialspace=True))
-  return MultiphaseCollector(reader, src_name, verbosity)
+    csv.reader(src, delimiter=field_delimiter, skipinitialspace=True))
+  result = MultiphaseCollector(reader, src_name, verbosity)
+  getattr(src, 'close', noop)()
+  return result
 
 
 def print_phase_results(multiphasecollector, number_format=''):
